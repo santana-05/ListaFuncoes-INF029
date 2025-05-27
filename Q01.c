cb14ae9028baf97b72f63321e600e0b9a2266ea0
Santana <mauriciosantanadasilva123@gmail.com>
2025-05-27 13:08:29 +0000

    Criação

---
+++ Q01.c
@@ -0,0 +1,22 @@
+/* Crie um programa que tenha uma função soma e a função main. A função main deve ler dois
+valores, enviar para a função soma. A função soma deve realizar a soma e retornar o valor. A
+Função main deve imprimir o resultado da soma.
+*/
+
+#include <stdio.h>
+
+int soma(int num1, int num2){
+    
+    return num1 + num2;    
+}
+
+int main() {
+    int x, y;
+    
+    scanf("%d", &x);
+    scanf("%d", &y);    
+    
+    printf("%d", soma(x,y));
+
+    return 0;
+}
---
+++ Q02.c
@@ -0,0 +1,24 @@
+/*Crie um programa que tenha uma função subtrai e a função main. A função main deve ler três
+valores, enviar para a função subtrai. A função subtrai deve realizar a subtração dos três valores
+(primeiro menos o segundo menos o terceiro) e retornar o valor. A Função main deve imprimir o
+resultado da subtração
+*/
+
+#include <stdio.h>
+
+int subtrair(int num1, int num2, int num3){
+    
+    return  (num1 - num2) - num3;
+}
+
+int main() {
+    int x, y, z;
+    
+    scanf("%d", &x);
+    scanf("%d", &y);
+    scanf("%d", &z);
+    
+    printf("%d", subtrair(x, y, z));
+
+    return 0;
+}
